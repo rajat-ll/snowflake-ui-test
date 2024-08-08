c95b6a4fbb5b9da941222decc10a51dac393f62f
rajat-ll <153066047+rajat-ll@users.noreply.github.com>
2024-08-08 15:59:44 +0530

    updated streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,13 @@ from snowflake.snowpark.context import get_active_session
 session = get_active_session()
 
 # Write directly to the app
-st.title("Intro ")
+st.title("Intro of POC ")
 
 # query = "SELECT * FROM LL_PROD_RAW_ZONE.PUBLIC.EXTRA_TABLES;"
 # df = pd.DataFrame(session.sql(query).collect())
 # st.write(df)
 
-st.write("Hello World I am Rajat Tiwari from India.........")
+st.write("Hello World I am Rajat Tiwari from India....Developing a automation of streamlit deployment")
 
 
 # dept_tables = pd.read_csv('table_dept_mapping.csv')
