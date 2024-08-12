26f9f999e76e65caba362e407d15d6bb5194c0a7
rajat-ll <153066047+rajat-ll@users.noreply.github.com>
2024-08-12 19:11:40 +0530

    table_dept_mapping.csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,21 @@
+import pandas as pd
+import numpy as np
+import snowflake.connector
+from datetime import datetime
 import streamlit as st
+import time
+from snowflake.connector.pandas_tools import write_pandas
+ 
 import login_page
 import data_edit_ui_page
 
 def main():
-    st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to", ["Login", "Edit UI"])
+    if 'current_page' not in st.session_state:
+        st.session_state.current_page = 'login'
 
-    if page == "Login":
+    if st.session_state.current_page == 'login':
         login_page.login()
-    elif page == "Edit UI":
+    elif st.session_state.current_page == 'editui':
         data_edit_ui_page.edit_ui()
 
 if __name__ == "__main__":
